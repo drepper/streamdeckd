896383be8b094e2bd9fdfd7f029afadd9c991e69
Ulrich Drepper <drepper@gmail.com>
2021-02-18 17:16:20 +0100

    More modern C++

--- obs.cc
+++ obs.cc
@@ -511,10 +511,9 @@ namespace obs {
         button_update(button_class::live | button_class::preview);
         break;
       case work_request::work_type::studiomode:
-        {
-          studio_mode = req.nr;
-          d["request-type"] = studio_mode ? "GetPreviewScene" : "GetCurrentScene";
-          auto res = obsws::call(d);
+        studio_mode = req.nr;
+        d["request-type"] = studio_mode ? "GetPreviewScene" : "GetCurrentScene";
+        if (auto res = obsws::call(d); res["status"] == "ok") {
           if (studio_mode)
             current_preview = res["name"].asString();
           current_sources.clear();
@@ -522,8 +521,8 @@ namespace obs {
             current_sources.emplace_back(s["name"].asString());
             current_sources.emplace_back(s["render"].asBool() ? "true"s : "false"s);
           }
-          button_update(button_class::all ^ button_class::live ^ button_class::record);
         }
+        button_update(button_class::all ^ button_class::live ^ button_class::record);
         break;
       case work_request::work_type::sourcename:
         for (size_t i = 0; 2 * i < current_sources.size(); ++i)
